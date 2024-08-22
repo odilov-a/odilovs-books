ea55bc241dc5ebfb694218dbfd6532b7df6f725b
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-08-22 21:54:27 +0500

    chore: Remove unused files and code

--- README.md
+++
@@ -1,1 +0,0 @@
-odilov's books
--- src/utilities/formatCurrency.ts
+++
@@ -1,5 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, { currency: "UZS", style: "currency" })
-
-export function formatCurrency(number: number) {
-  return CURRENCY_FORMATTER.format(number)
-}
